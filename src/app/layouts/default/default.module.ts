f8086b680e856147a53e0a93b04664ba163014bb
AgrebiOmar123 <oagrebi614@gmail.com>
2023-11-21 11:29:07 +0100

    dashbord

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashbordComponent } from 'src/app/modules/dashbord/dashbord.component';
+import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule } from '@angular/material/divider';
+import {MatCardModule } from '@angular/material/card';
+import {MatPaginatorModule } from '@angular/material/paginator';
+import {MatTableModule } from '@angular/material/table';
+import { FlexLayoutModule } from '@angular/flex-layout'; 
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashbordComponent,
+    PostsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatTableModule,
+    FlexLayoutModule,
+    
+    
+   
+  ]
+})
+export class DefaultModule { }
